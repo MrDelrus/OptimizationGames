4e404a271ee937dbef0c4513cdb3ea8eedcb7c7c
Nikita Zolin <zolin.na@phystech.edu>
2024-09-07 23:14:06 +0300

    fix hierarchy

--- 1. Game of Trust/1. Game of Trust/strategies/splitcat.cpp
+++
@@ -1,9 +0,0 @@
-class A : public Participant { // Splitcat
-public:
-    explicit A(int number) {}
-    void StartNewGame() override {}
-    Move MakeMove() override {
-        return Move::SPLIT;
-    }
-    void GetOpponentMove(Move) override {}
-};
--- 1. Game of Trust/1. Game of Trust/strategies/stealcat.cpp
+++
@@ -1,9 +0,0 @@
-class A : public Participant { // Stealcat
-public:
-    explicit A(int number) {}
-    void StartNewGame() override {}
-    Move MakeMove() override {
-        return Move::STEAL;
-    }
-    void GetOpponentMove(Move) override {}
-};
